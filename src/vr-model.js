d4319f136dceea6bab08a22425b901af95eca753
Diego Marcos <diego.marcos@gmail.com>
2015-05-25 15:24:49 -0700

    Exploding cubes ^.^

--- src/vr-model.js
+++ src/vr-model.js
@@ -39,11 +39,139 @@ module.exports = component.register('vr-model', {
     var x = this.getAttribute('x') || 0;
     var y = this.getAttribute('y') || 0;
     var z = this.getAttribute('z') || 0;
+    this.raycaster = new THREE.Raycaster();
     model.overdraw = true;
     model.position.set(x, y, z);
     this.object3D = model;
     this.scene.addObject(this);
+    this.attachClickHandler();
     this.animate();
+  },
+
+  attachClickHandler: function() {
+    var self = this;
+    self.mousePos = new THREE.Vector2(0, 0);
+    this.scene.addEventListener('mousemove', onMouseMoved, false);
+    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+
+    function onMouseMoved ( e ) {
+      e.preventDefault();
+      self.mousePos.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+      self.mousePos.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+    }
+
+    function onDocumentMouseDown( e ) {
+      if (self.intersected) {
+        self.explode();
+      }
+      // e.preventDefault();
+      // var mouseVector = new THREE.Vector3();
+      // mouseVector.x = 2 * (e.clientX / SCREEN_WIDTH) - 1;
+      // mouseVector.y = 1 - 2 * ( e.clientY / SCREEN_HEIGHT );
+      // var raycaster = projector.pickingRay( mouseVector.clone(), camera );
+      // var intersects = raycaster.intersectObject( TARGET );
+      // for( var i = 0; i < intersects.length; i++ ) {
+      //   var intersection = intersects[ i ],
+      //   obj = intersection.object;
+      //   cons ole.log("Intersected object", obj);
+      // }
+    }
+  },
+
+  explode: function() {
+
+    var box = this.object3D;
+    var scene = this.scene;
+    var duration = 8000;
+    this.exploding = true;
+
+    // explode geometry into objects
+    var pieces = explode( box.geometry, box.material );
+
+    box.material.visible = false;
+
+    // animate objects
+    for ( var i = 0; i < pieces.children.length; i ++ ) {
+
+      var object = pieces.children[ i ];
+
+      object.geometry.computeFaceNormals();
+      var normal = object.geometry.faces[0].normal.clone();
+      var targetPosition = object.position.clone().add( normal.multiplyScalar( 3000 ) );
+      //removeBoxFromList( box );
+      new TWEEN.Tween( object.position )
+        .to( targetPosition, duration )
+        .onComplete( deleteBox )
+        .start();
+
+      object.material.opacity = 0;
+      new TWEEN.Tween( object.material )
+        .to( { opacity: 1 }, duration )
+        .start();
+
+      var rotation = 2 * Math.PI;
+      var targetRotation = { x: rotation, y: rotation, z:rotation };
+      new TWEEN.Tween( object.rotation )
+        .to( targetRotation, duration )
+        .start();
+
+    }
+
+    box.add( pieces );
+
+    function removeBoxFromList( box ) {
+      for (var i = 0; i < objects.length; i++) {
+        if (objects[i] === box) {
+          objects.splice(i, 1);
+          return;
+        }
+      }
+    }
+
+    function deleteBox() {
+      box.remove( pieces )
+      //scene.remove( box );
+    }
+
+    function explode( geometry, material ) {
+
+      var pieces = new THREE.Group();
+      var material = material.clone();
+      material.side = THREE.DoubleSide;
+
+      for ( var i = 0; i < geometry.faces.length; i ++ ) {
+
+        var face = geometry.faces[ i ];
+
+        var vertexA = geometry.vertices[ face.a ].clone();
+        var vertexB = geometry.vertices[ face.b ].clone();
+        var vertexC = geometry.vertices[ face.c ].clone();
+
+        var geometry2 = new THREE.Geometry();
+        geometry2.vertices.push( vertexA, vertexB, vertexC );
+        geometry2.faces.push( new THREE.Face3( 0, 1, 2 ) );
+
+        var mesh = new THREE.Mesh( geometry2, material );
+        mesh.position.sub( geometry2.center() );
+        pieces.add( mesh );
+
+      }
+
+      //sort the pieces
+      pieces.children.sort( function ( a, b ) {
+
+        return a.position.z - b.position.z;
+        //return a.position.x - b.position.x;     // sort x
+        //return b.position.y - a.position.y;   // sort y
+
+      } );
+
+      pieces.rotation.set( 0, 0, 0 )
+
+      return pieces;
+
+    }
+
   },
 
   animate: function() {
@@ -52,13 +180,41 @@ module.exports = component.register('vr-model', {
     var angularSpeed = self.angularSpeed || 0.2;
     requestAnimationFrame(function() {
       self.animate();
+      TWEEN.update();
     });
 
-    var time = (new Date()).getTime();
-    var timeDiff = time - lastTime;
-    var angleChange = angularSpeed * timeDiff * 2 * Math.PI / 1000;
-    self.model.rotation.y += angleChange;
-    self.lastTime = time;
+    if (!this.exploding) {
+      var time = (new Date()).getTime();
+      var timeDiff = time - lastTime;
+      var angleChange = angularSpeed * timeDiff * 2 * Math.PI / 1000;
+      self.model.rotation.y += angleChange;
+      self.lastTime = time;
+      this.intersectMouse();
+    }
+  },
+
+  // find intersections
+  intersectMouse: function intersect() {
+    var raycaster = this.raycaster;
+    var objects = [this.object3D];
+    raycaster.setFromCamera( this.mousePos, this.scene.camera );
+    var intersects = raycaster.intersectObjects( objects );
+
+    if ( intersects.length > 0 ) {
+
+      if ( this.object3D == intersects[ 0 ].object && !this.intersected) {
+
+        this.intersected = this.object3D.material.emissive.getHex();
+        this.object3D.material.emissive.setHex( 0xffff00 );
+
+      }
+
+    } else {
+
+      if ( this.intersected ) this.object3D.material.emissive.set( 'black' );
+      this.intersected = null;
+
+    }
   },
 
   attributeChanged: function(name, from, to) {
